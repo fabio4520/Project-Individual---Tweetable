af94b88a6fd3008ec5726bdefb0fbc5ad9985b44
fabio4520 <fabioleofc@gmail.com>
2022-05-06 12:40:21 -0500

    ADD: avatar in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # Validations
+  
+  has_one_attached :avatar 
+  
+         # Validations
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
   validates :name, presence: true
+
+  # Associations
+
 end

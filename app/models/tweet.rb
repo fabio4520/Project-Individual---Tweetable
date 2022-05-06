19b56ab48f876b903e6abc1a46b242c662792b92
fabio4520 <fabioleofc@gmail.com>
2022-05-06 12:48:21 -0500

    ADD: Associations for Tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,6 @@ class Tweet < ApplicationRecord
   belongs_to :user
   # Validations
   validates :body, presence: true, length: { maximum: 140 }
+  # Associations
+  has_many :comments, dependent: :destroy
 end
